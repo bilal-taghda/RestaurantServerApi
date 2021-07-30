e8a20dbbe073abf800b05c13c8f45242f8769b7a
bilaltaghda5@gmail.com <Taghdataghda123>
2021-07-30 11:01:56 +0100

    configured heroku postgres db

--- src/repository/DatabaseFactory.kt
+++ src/repository/DatabaseFactory.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -29,17 +30,19 @@ object DatabaseFactory {
     fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER")
-        config.jdbcUrl ="jdbc:postgresql://localhost:5432/postgres"
-        config.username = System.getenv("USERNAME")
-        config.password = System.getenv("PASSWORD")
+      //  config.jdbcUrl ="jdbc:postgresql://localhost:5432/postgres"
+        //config.username = System.getenv("USERNAME")
+        //config.password = System.getenv("PASSWORD")
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
 
-   /*     val uri = URI(System.getenv("DATABASE_URL"))
+        val uri = URI(System.getenv("DATABASE_URL"))
         val username = uri.userInfo.split(":").toTypedArray()[0]
         val password = uri.userInfo.split(":").toTypedArray()[1]
-*/
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
+
 
 
         config.validate()
